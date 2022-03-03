8c71e6a3dae341b2e34dedc8ab02a8c02e81bf9c
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-03 14:32:55 -0800

    created the needed todos for the project.

---
+++ amazon_price_tracker.py
@@ -0,0 +1,13 @@
+# Name: Ruben Sanduleac
+# Date: March 3, 2022
+# Description: This program uses BeautifulSoup to scrape the price of a specified item from
+#              Amazon website. The specifies a target price and the program compares the actual price of
+#              to it. Once the price is at or bellow the target price the user is notified with an email.
+
+
+# TODO 1: Find an URL to track
+# TODO 2: Use the http://myhttpheader.com/ to find the header needed
+# TODO 3: Use the requests library to request the HTML page of the Amazon product using the URL
+# TODO 4: Use BeautifulSoup to make soup with the web page HTML you get back. You'll need to use the "lxml"
+#      parser instead of the "html.parser" for this to work.
+# TODO 5: Use BeautifulSoup to get hold of the price of the item as a floating point number and print it out.
